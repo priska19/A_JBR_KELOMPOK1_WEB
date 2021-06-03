0c4c33dd276240761a6bc5c810a39778ea018ee4
A_E41190139_LigaBayuHerdianto <71804899+LigaBayuHerdianto@users.noreply.github.com>
2021-06-03 19:56:50 +0700

    Summary

---
+++ Bakso_viral/resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                Copyright @2021 Kelompok 1 Jember - Bakso Viral Jember
+            </div>
+        </div>
+    </div>
+</footer>
