90b28da0e1e6f2899723efdd268165ba7ae5c46a
A_E41190139_LigaBayuHerdianto <71804899+LigaBayuHerdianto@users.noreply.github.com>
2021-06-09 21:44:20 +0700

    Summary

--- Bakso_viral/resources/views/backend/layouts/sidebar.blade.php
+++ Bakso_viral/resources/views/backend/layouts/sidebar.blade.php
@@ -12,8 +12,7 @@
           <li class="sub-menu">
             <a href="javascript:;" class="">
                           <i class="icon_document_alt"></i>
-                          <span>Forms</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
+                          <span>List Menu</span>
                       </a>
             <ul class="sub">
               <li><a class="" href="form_component.html">Form Elements</a></li>
@@ -23,8 +22,7 @@
           <li class="sub-menu">
             <a href="javascript:;" class="">
                           <i class="icon_desktop"></i>
-                          <span>UI Fitures</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
+                          <span>Laporan History</span>
                       </a>
             <ul class="sub">
               <li><a class="" href="general.html">Elements</a></li>
@@ -35,45 +33,17 @@
           <li>
             <a class="" href="widgets.html">
                           <i class="icon_genius"></i>
-                          <span>Widgets</span>
+                          <span>Admin</span>
                       </a>
           </li>
           <li>
             <a class="" href="chart-chartjs.html">
                           <i class="icon_piechart"></i>
-                          <span>Charts</span>
+                          <span>Laporan</span>
 
                       </a>
 
           </li>
-
-          <li class="sub-menu">
-            <a href="javascript:;" class="">
-                          <i class="icon_table"></i>
-                          <span>Tables</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
-                      </a>
-            <ul class="sub">
-              <li><a class="" href="basic_table.html">Basic Table</a></li>
-            </ul>
-          </li>
-
-          <li class="sub-menu">
-            <a href="javascript:;" class="">
-                          <i class="icon_documents_alt"></i>
-                          <span>Pages</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
-                      </a>
-            <ul class="sub">
-              <li><a class="" href="profile.html">Profile</a></li>
-              <li><a class="" href="login.html"><span>Login Page</span></a></li>
-              <li><a class="" href="contact.html"><span>Contact Page</span></a></li>
-              <li><a class="" href="blank.html">Blank Page</a></li>
-              <li><a class="" href="404.html">404 Error</a></li>
-            </ul>
-          </li>
-
-        </ul>
         <!-- sidebar menu end-->
       </div>
     </aside>
